3a628fd6a8c29a177139cee3f1273890be4741a4
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-06-10 19:24:07 +0600

    Added words_to_capital() function in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def str_to_upper(phrase: str):
+    """Переводим строку в заглавные буквы"""
     return phrase.upper()
+
+
+def words_to_capital(phrase: str):
+    """Функция переводит все слова во фразе с заглавной буквы"""
+    result = []
+    words_in_list = phrase.split()
+    for word in words_in_list:
+        result.append(word.capitalize())
+    return result
